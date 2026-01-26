a01ad3125e59dacf5049cc0882b4ce30f25b9ce5
suraj vishwakarma <dev.surajv@gmail.com>
2026-01-26 18:14:55 +0000

    refactor: remove unused Input import from editor-controls

--- components/profile-editor/editor-controls.tsx
+++ components/profile-editor/editor-controls.tsx
@@ -4,7 +4,6 @@ import { Slider } from '@/components/ui/slider'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { Input } from '@/components/ui/input'
 import { BackgroundPreset } from './types'
 import { Move, Palette, Box } from 'lucide-react'
 
